5f9c47a801c630f66d9da217631690eb2d733f47
vd <vadyur@csodessa.com>
2021-11-24 11:32:51 +0200

    Timezone support

--- lib/f1base.py
+++ lib/f1base.py
@@ -1,5 +1,9 @@
 import re
 
+import calendar
+import time
+from datetime import datetime,tzinfo,timedelta
+
 def current_year():
 	return 2021
 
@@ -16,3 +20,37 @@ def gp_variants(GP):
 	if alt:
 		return '{} | {}'.format(GP, alt)
 	return GP
+
+def local_time_from_msk(msk_dt):
+	class Zone(tzinfo):
+		def __init__(self,offset,isdst,name):
+			self.offset = offset
+			self.isdst = isdst
+			self.name = name
+		def utcoffset(self, dt):
+			return timedelta(hours=self.offset) + self.dst(dt)
+		def dst(self, dt):
+			return timedelta(hours=1) if self.isdst else timedelta(0)
+		def tzname(self,dt):
+			return self.name	
+
+	t = time.time()
+	lt = time.localtime(t)
+
+	if hasattr(lt, 'tm_gmtoff'):
+		TZ_OFFSET = lt.tm_gmtoff // 3600
+	else:
+		gmt = time.gmtime(t)
+		offset = time.mktime(lt) - time.mktime(gmt)
+		TZ_OFFSET = offset // 3600
+
+	MSK = Zone(3,False,'MSK')	
+	LOCAL = Zone(TZ_OFFSET, False, 'LOCAL')
+
+	format = '%H:%M'
+	dt = datetime.strptime(msk_dt, format)
+	dt = dt.replace(tzinfo=MSK)
+	dt = dt.astimezone(LOCAL)
+	result = datetime.strftime(dt, format)
+	return result
+
